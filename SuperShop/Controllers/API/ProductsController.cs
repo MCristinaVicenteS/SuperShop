35df05b47ec711af1d0cc381b391ad1d0b42fd28
Maria Silva <Maria.Silva.22159@formandos.cinel.pt>
2023-05-30 15:47:06 +0100

    API products controller

---
+++ SuperShop/Controllers/API/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperShop.Data;
+
+namespace SuperShop.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        //receber os produtos todos do repository -> com GET
+        [HttpGet]
+        public ActionResult GetProducts()
+        {
+            return Ok(_productRepository.GetAll()); //dados em json através da função Ok
+        }
+    }
+}
