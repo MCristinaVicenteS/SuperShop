f7777b17ca0b559872ad04d0f7c8031967e32610
Maria Silva <Maria.Silva.22159@formandos.cinel.pt>
2023-06-19 11:37:44 +0100

    Login and logout

---
+++ SuperShop/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperShop.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Username { get; set; } //vai corresponder ao email
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
